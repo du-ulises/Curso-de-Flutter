711bb335eac92f405c4ad3220a7cff1b37efb0e0
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-11 14:37:36 -0500

    Widget Image

---
+++ platzi_trips_app/lib/review.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Review extends StatelessWidget{
+
+  String pathImage = "assets/images/people.jpg";
+
+
+  Review(this.pathImage);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final photo = Container(
+      margin: EdgeInsets.only(
+        top: 20,
+        left: 20
+      ),
+      width: 80,
+      height: 80,
+
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(pathImage)
+        )
+      ),
+    );
+
+    return Row(
+      children: <Widget>[
+
+      ],
+    );
+  }
+
+}
