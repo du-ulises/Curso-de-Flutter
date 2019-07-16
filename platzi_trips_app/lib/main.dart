b6a2f6a055d1e332f3a39285dbdd40c686a1889c
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-16 14:32:21 -0500

    Personalizando nuestro BottomNavigation Bar a Cupertino iOS BottomBar

--- platzi_trips_app/lib/main.dart
+++ platzi_trips_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'platzi_trips.dart';
+import 'platzi_trips_cupertino.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: PlatziTrips()
+      home: PlatziTripsCupertino()
       //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
