48567ce1e6a429476ecee3bd77edb246954bd696
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-15 22:50:35 -0500

    Widget Button, InkWell

--- platzi_trips_app/lib/description_place.dart
+++ platzi_trips_app/lib/description_place.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'button_purple.dart';
 
 class DescriptionPlace extends StatelessWidget{
 
@@ -93,9 +94,11 @@ class DescriptionPlace extends StatelessWidget{
     );
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         title_stars,
-        description
+        description,
+        ButtonPurple("Navigate")
       ],
     );
   }
