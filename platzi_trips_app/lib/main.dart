92d1adc4ff5b564c2f50b79f2a6c3d3398f1d4bc
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-16 14:14:40 -0500

    Generando Navegación en BottomNavigationBar

--- platzi_trips_app/lib/main.dart
+++ platzi_trips_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'platzi_trips.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: //MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PlatziTrips()
+      //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
